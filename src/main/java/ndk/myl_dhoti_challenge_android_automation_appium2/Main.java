7cc7a161b4fb395a9071e2fcb68ad69d26fe32ba
Banee Ishaque K <baneeishaque@gmail.com>
2022-12-31 02:46:42 +0530

    district typed ok

--- src/main/java/ndk/myl_dhoti_challenge_android_automation_appium2/Main.java
+++ src/main/java/ndk/myl_dhoti_challenge_android_automation_appium2/Main.java
@@ -1,7 +1,10 @@
 package ndk.myl_dhoti_challenge_android_automation_appium2;
 
+//import io.appium.java_client.TouchAction;
+
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -28,7 +31,13 @@ public class Main {
 
             AndroidDriver remoteWebDriver = new AndroidDriver(new URI("http://127.0.0.1:4723/").toURL(), desiredCapabilities);
             remoteWebDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
-            remoteWebDriver.findElement(By.xpath("//android.widget.ImageView[@content-desc=\"Report\"]")).click();
+            remoteWebDriver.findElement(By.xpath("//android.widget.ImageView[@content-desc='Report']")).click();
+            remoteWebDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+            WebElement districtElement = remoteWebDriver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]"));
+            districtElement.click();
+            districtElement.sendKeys("MALAPPURAM");
+//            TouchAction touchAction = new TouchAction(remoteWebDriver);
+//            touchAction.tap(PointOption.point(districtElement.getLocation().moveBy(5, 2))).perform();
 
         } catch (MalformedURLException | URISyntaxException e) {
 
