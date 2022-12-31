bff512fb11ec4bb0006e4891897fbb1c95ce572c
Banee Ishaque K <baneeishaque@gmail.com>
2022-12-31 16:42:12 +0530

    CSV Reader

--- src/main/java/ndk/csv_processor/Csv_Parser.java
+++ src/main/java/ndk/csv_processor/Csv_Parser.java
@@ -13,9 +13,33 @@ public class Csv_Parser {
             reader = new CSVReader(new FileReader("Malappuram_Tanalur_Pandiyatt.csv"));
             String[] nextLine;
             reader.readNext();
+            int offset = 0;
             while ((nextLine = reader.readNext()) != null) {
-                for (int i = 0; i < nextLine.length; i++) {
-                    System.out.print(nextLine[i] + "\t");
+
+                nextLine[0] = String.valueOf(offset + Integer.parseInt(nextLine[0]));
+                int count = Integer.parseInt(nextLine[9]);
+                if (count > 1) {
+                    for (int i = 1; i <= count; i++) {
+                        if (i != 1) {
+                            System.out.println();
+                            nextLine[1] = nextLine[1].substring(0, nextLine[1].length() - 2) + " " + i;
+                        } else {
+                            nextLine[1] = nextLine[1] + " " + i;
+                        }
+                        nextLine[8] = String.valueOf(600);
+                        nextLine[9] = String.valueOf(1);
+                        for (int j = 0; j < nextLine.length; j++) {
+
+                            System.out.print(nextLine[j] + "\t");
+                        }
+                        nextLine[0] = String.valueOf(Integer.parseInt(nextLine[0]) + 1);
+                    }
+                    offset = offset + (count - 1);
+                } else {
+                    for (int j = 0; j < nextLine.length; j++) {
+
+                        System.out.print(nextLine[j] + "\t");
+                    }
                 }
                 System.out.println();
             }
